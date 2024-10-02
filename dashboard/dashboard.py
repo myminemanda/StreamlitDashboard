f15aadceafc853242f73f37473256b3e8b8ed4f5
ml52manda <m144b4kx4042@bangkit.academy>
2024-10-02 08:33:51 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -82,7 +82,7 @@ max_date = all_df["dteday"].max()
 
 with st.sidebar:
     # Menambahkan logo perusahaan
-    st.image("D:\\Dashboard\\bikelogo.jpg")
+    st.image("dashboard/bikelogo.jpg")
 
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
